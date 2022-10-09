6cd2e06427f9c080c8234b1b5c37d0abd9d18d28
Hugo Briatte <hugobriatte@otmail.fr>
2022-10-09 12:55:33 +0200

    Correction de comportements avec les joueurs

--- js/reader.js
+++ js/reader.js
@@ -9,10 +9,10 @@ var currentQuestion = undefined
 function initGame(file) {
     var reader = new FileReader();
     reader.onload = function(evt) {
-        defaultJSON = JSON.parse(evt.target.result)
-        console.log(defaultJSON)
-        json = defaultJSON
-        addGame(defaultJSON)
+        defaultJSON = JSON.parse(evt.target.result);
+        console.log(defaultJSON);
+        json = defaultJSON;
+        addGame(defaultJSON);
     };
     reader.readAsText(file);
 }
@@ -22,10 +22,10 @@ function initGame(file) {
  * @param {Array} games : Un tableau contenant une série de jeux 
  */
 function addGame(games) {
-    const div = document.getElementById("cardZone")
-    var i = 0
+    const div = document.getElementById("cardZone");
+    var i = 0;
 
-    div.innerHTML = ""
+    div.innerHTML = "";
 
     games.forEach(game => {
         div.innerHTML += `<div class="card" onclick="showGame(${i})">\
@@ -41,83 +41,90 @@ function addGame(games) {
  * @param {number} id L'ID du jeu (sa position dans le tableau de jeux)
  */
 function showGame(id) {
-    const div = document.getElementById("questionZone")
-    const card = document.getElementById("questionInner")
+    const div = document.getElementById("questionZone");
+    const card = document.getElementById("questionInner");
 
     card.innerHTML = `<div id="questionTitle">${json[id].name}</div>\
-                    <div id="questionRule">${json[id].rules}</div>`
+                    <div id="questionRule">${json[id].rules}</div>`;
 
-    div.style.top = "0%"
-    currentGame = json[id]
-    currentQuestion = 0
+    div.style.top = "0%";
+    currentGame = json[id];
+    currentQuestion = 0;
 
-    document.getElementById("previous").style.left = "-100%"
-    document.getElementById("next").style.right = "0%"
+    document.getElementById("previous").style.left = "-100%";
+    document.getElementById("next").style.right = "0%";
 }
 
 /**
  * Cache la zone de question
  */
 function hideQuestion() {
-    const div = document.getElementById("questionZone")
+    const div = document.getElementById("questionZone");
 
-    div.style.top = "-100%"
+    div.style.top = "-100%";
 }
 
 /**
  * Ajoute un joueur dans la liste des joueurs
  */
 function addPlayer() {
-    var div = document.getElementById("playerZone")
+    var div = document.getElementById("playerZone");
 
-    var newPlayer = document.createElement("div")
-    newPlayer.classList.add("player")
-    if (div.childElementCount == 0)
-        newPlayer.classList.add("current")
+    var newPlayer = document.createElement("div");
+    newPlayer.classList.add("player");
+    if (div.childNodes.length == 0)
+        newPlayer.classList.add("current");
 
-    var newName = document.createElement("input")
-    newName.type = "text"
-    newName.classList.add("name")
-    newName.value = "Name"
-    newName.style.fontWeight = "bold"
-    newName.style.color = "#c45118"
-    newPlayer.appendChild(newName)
+    var newName = document.createElement("input");
+    newName.type = "text";
+    newName.classList.add("name");
+    newName.value = "Name";
+    newName.style.fontWeight = "bold";
+    newName.style.color = "#c45118";
+    newPlayer.appendChild(newName);
 
-    var newScore = document.createElement("input")
-    newScore.type = "number"
-    newScore.classList.add("score")
-    newScore.value = "0"
-    newScore.style.fontSize = "20px"
-    newPlayer.appendChild(newScore)
+    var newScore = document.createElement("input");
+    newScore.type = "number";
+    newScore.classList.add("score");
+    newScore.value = "0";
+    newScore.style.fontSize = "20px";
+    newPlayer.appendChild(newScore);
 
-    div.appendChild(newPlayer)
+    div.appendChild(newPlayer);
 }
 
 /**
- * Supprime un joueur de la liste des joueurs
+ * Supprime le joueur actuellement sélectionné de la liste des joueurs
  */
 function removePlayer() {
-    var div = document.getElementById("playerZone")
+    var div = document.getElementById("playerZone");
+    var i = 0;
 
-    try {
-        if (div.lastChild.classList.contains("current"))
-            nextPlayer()
-        div.removeChild(div.lastChild)
-    } catch (e) {}
+    while(i < div.childNodes.length && !div.childNodes[i].classList.contains("current")) {
+        i += 1;
+    }
+    if(i < div.childNodes.length) {
+        div.childNodes[(i + 1) % div.childNodes.length].classList.add("current");
+        div.removeChild(div.childNodes[i]);
+    }
 }
 
 /**
  * Passe le joueur actuel
- * @returns 
  */
 function nextPlayer() {
-    var div = document.getElementById("playerZone")
-    var current = div.getElementsByClassName("current")[0]
-    try { var next = current.nextElementSibling } catch (e) { return }
-    if (next == null)
-        next = div.firstElementChild
-    current.classList.remove("current")
-    next.classList.add("current")
+    var div = document.getElementById("playerZone");
+    var current = div.getElementsByClassName("current")[0];
+    var next = null;
+    if(current == null) {
+        next = div.firstElementChild;
+    } else {
+        try { var next = current.nextElementSibling; } catch (e) { }
+        if (next == null)
+            next = div.firstElementChild;
+        current.classList.remove("current");
+    }
+    next.classList.add("current");
 }
 
 /**
@@ -125,31 +132,31 @@ function nextPlayer() {
  * @param {number} cursor Le nombre de question à passer
  */
 function showContent(cursor) {
-    const card = document.getElementById("questionInner")
-    const prev = document.getElementById("previous")
-    const next = document.getElementById("next")
+    const card = document.getElementById("questionInner");
+    const prev = document.getElementById("previous");
+    const next = document.getElementById("next");
 
-    currentQuestion += cursor
+    currentQuestion += cursor;
 
-    card.innerHTML = `<div id="questionTitle">Question n°${(currentQuestion)}</div>`
+    card.innerHTML = `<div id="questionTitle">Question n°${(currentQuestion)}</div>`;
 
     if (currentGame.content[currentQuestion - 1].question != undefined)
-        card.innerHTML += `<div id="questionQuestion">${currentGame.content[currentQuestion - 1].question}</div>`
+        card.innerHTML += `<div id="questionQuestion">${currentGame.content[currentQuestion - 1].question}</div>`;
 
     switch (currentGame.content[currentQuestion - 1].type) {
         case "text":
-            card.innerHTML += `<div id="questionRule">${currentGame.content[currentQuestion - 1].content}</div>`
-            break
+            card.innerHTML += `<div id="questionRule">${currentGame.content[currentQuestion - 1].content}</div>`;
+            break;
         case "picture":
-            card.innerHTML += `<div id="questionRule"><img src="${currentGame.content[currentQuestion - 1].content}"></div>`
-            break
+            card.innerHTML += `<div id="questionRule"><img src="${currentGame.content[currentQuestion - 1].content}"></div>`;
+            break;
         case "audio":
-            card.innerHTML += `<div id="questionRule"><audio controls><source src="${currentGame.content[currentQuestion - 1].content}" type="audio/mpeg"></audio></div>`
-            break
+            card.innerHTML += `<div id="questionRule"><audio controls><source src="${currentGame.content[currentQuestion - 1].content}" type="audio/mpeg"></audio></div>`;
+            break;
     }
 
-    card.innerHTML += `<div id="questionAnswer" tabindex="0">${currentGame.content[currentQuestion - 1].answer}</div>`
+    card.innerHTML += `<div id="questionAnswer" tabindex="0">${currentGame.content[currentQuestion - 1].answer}</div>`;
 
-    if (currentQuestion <= 1) { prev.style.left = "-100%" } else { prev.style.left = "0%" }
-    if (currentQuestion >= currentGame.content.length) { next.style.right = "-100%" } else { next.style.right = "0%" }
+    if (currentQuestion <= 1) { prev.style.left = "-100%"; } else { prev.style.left = "0%"; }
+    if (currentQuestion >= currentGame.content.length) { next.style.right = "-100%"; } else { next.style.right = "0%"; }
 }
